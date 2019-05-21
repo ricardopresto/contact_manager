43fbf68bda727ccf48a92ae72ec9237d0fca94cc
ricardopresto <ricardopresto5@gmail.com>
2019-05-21 17:20:51 +0200

    Updates

---
+++ precache-manifest.0aee9fca9ca39f1133a1e23d260b567b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a386728f6496e818884903ed2ebfa7cd",
+    "url": "/contact_manager/index.html"
+  },
+  {
+    "revision": "63c931a45b16531ef618",
+    "url": "/contact_manager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "0e52da2ea5cca74de241",
+    "url": "/contact_manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "63c931a45b16531ef618",
+    "url": "/contact_manager/static/js/2.760bc175.chunk.js"
+  },
+  {
+    "revision": "0e52da2ea5cca74de241",
+    "url": "/contact_manager/static/js/main.1e6df5d8.chunk.js"
+  },
+  {
+    "revision": "2b941c4b3b0d1aff9d5e",
+    "url": "/contact_manager/static/js/runtime~main.75798bfb.js"
+  }
+]);
